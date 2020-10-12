e5979f8ace117b85988e12efc92e83f7c9f6cfb7
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-12 20:00:24 +0700

    Membuat validator signin

---
+++ app/Validators/SignIn.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class SignIn {
+  get rules() {
+    return {
+      username: 'required',
+      password: 'required'
+    };
+  }
+
+  get validateAll() {
+    return true;
+  }
+}
+
+module.exports = SignIn;
