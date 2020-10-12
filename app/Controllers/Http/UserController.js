ffbde2f8378473ffc34301d100a2e0c8c50bd6be
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-12 19:57:25 +0700

    Membuat method signup -> controller user

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,93 +1,27 @@
-'use strict'
+'use strict';
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Hash = use('Hash');
+const Token = use('App/Models/Token');
+const User = use('App/Models/User');
 
-/**
- * Resourceful controller for interacting with users
- */
 class UserController {
-  /**
-   * Show a list of all users.
-   * GET users
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
+  async signUp({ request, auth }) {
+    const data = request.post();
+    data.username = await data.username.replace(/\s+/g, '');
+    data.password = await Hash.make(data.password);
+    data.profile_picture = `https://ui-avatars.com/api/?name=${data.username}`;
 
-  /**
-   * Render a form to be used for creating a new user.
-   * GET users/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+    const user = await User.create(data);
+    user.token = await auth.generate(user);
 
-  /**
-   * Create/save a new user.
-   * POST users
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
+    const token = new Token();
+    token.user_id = user.id;
+    token.type = user.token.type;
+    token.token = user.token.token;
+    await token.save();
 
-  /**
-   * Display a single user.
-   * GET users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing user.
-   * GET users/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
-
-  /**
-   * Update user details.
-   * PUT or PATCH users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a user with id.
-   * DELETE users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+    return user;
   }
 }
 
-module.exports = UserController
+module.exports = UserController;
