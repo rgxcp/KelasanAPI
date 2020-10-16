1b4a3a99f3e7f280ab357b6e4c482013316f8086
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-16 22:39:21 +0700

    Mengubah logic method signup -> controller user

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,21 +1,14 @@
 'use strict'
 
-const Token = use('App/Models/Token')
 const User = use('App/Models/User')
 
 class UserController {
   async signUp({ request, auth }) {
-    const data = request.post()
+    const data = await request.post()
     data.profile_picture = `https://ui-avatars.com/api/?name=${data.email}`
 
     const user = await User.create(data)
     user.token = await auth.generate(user)
-
-    const token = new Token()
-    token.user_id = user.id
-    token.type = user.token.type
-    token.token = user.token.token
-    await token.save()
 
     return user
   }
