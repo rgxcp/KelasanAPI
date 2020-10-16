fe2d1d46ef16e8c46814ecfd601b93df4edd6e3f
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-16 22:13:09 +0700

    Menyesuaikan model user dengan boilerplate

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,10 +1,25 @@
 'use strict'
 
+const Hash = use('Hash')
 const Model = use('Model')
 
 class User extends Model {
   static get hidden() {
     return ['password']
+  }
+
+  static boot() {
+    super.boot()
+
+    this.addHook('beforeSave', async (userInstance) => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password)
+      }
+    })
+  }
+
+  tokens() {
+    return this.hasMany('App/Models/Token')
   }
 }
 
