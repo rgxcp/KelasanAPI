7392aef8bbee5d2039303cb60c03f71de77f29bf
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-27 20:26:26 +0700

    Mengisi kerangka migrasi completed_assignment

--- database/migrations/1603802082502_completed_assignment_schema.js
+++ database/migrations/1603802082502_completed_assignment_schema.js
@@ -5,8 +5,21 @@ const Schema = use('Schema')
 class CompletedAssignmentSchema extends Schema {
   up() {
     this.create('completed_assignments', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('assignment_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('assignments')
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
