8d78a5d3f55c1662f4b48d7bc91f2f3ece5e387c
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-28 02:02:05 +0700

    Mengisi kerangka migrasi attachment

--- database/migrations/1603807735448_assignment_attachment_schema.js
+++ database/migrations/1603807735448_assignment_attachment_schema.js
@@ -5,8 +5,16 @@ const Schema = use('Schema')
 class AssignmentAttachmentSchema extends Schema {
   up() {
     this.create('assignment_attachments', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('assignment_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('assignments')
+      table.string('direct_link').notNullable().collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
--- database/migrations/1603807744335_note_attachment_schema.js
+++ database/migrations/1603807744335_note_attachment_schema.js
@@ -5,8 +5,16 @@ const Schema = use('Schema')
 class NoteAttachmentSchema extends Schema {
   up() {
     this.create('note_attachments', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('note_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('notes')
+      table.string('direct_link').notNullable().collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
