c0de72f7831e56e1cf2fbb91fc91d074203b0fab
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-27 20:17:35 +0700

    Mengisi kerangka migrasi subject

--- database/migrations/1603802067954_subject_schema.js
+++ database/migrations/1603802067954_subject_schema.js
@@ -5,8 +5,16 @@ const Schema = use('Schema')
 class SubjectSchema extends Schema {
   up() {
     this.create('subjects', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('classroom_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('classrooms')
+      table.string('name', 50).notNullable().collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
