1f89e1272606341774d5c7f65994fef23baef411
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-27 19:59:06 +0700

    Mengisi kerangka migrasi folder

--- database/migrations/1603802043587_folder_schema.js
+++ database/migrations/1603802043587_folder_schema.js
@@ -5,8 +5,16 @@ const Schema = use('Schema')
 class FolderSchema extends Schema {
   up() {
     this.create('folders', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('classroom_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('classrooms')
+      table.string('name', 20).notNullable().collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
