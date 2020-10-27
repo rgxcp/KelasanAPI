ab7a9421a708ee3aabaa9521ce228d4afb88a212
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-27 21:06:36 +0700

    Mengisi kerangka migrasi note_timeline

--- database/migrations/1603802096749_note_timeline_schema.js
+++ database/migrations/1603802096749_note_timeline_schema.js
@@ -5,8 +5,25 @@ const Schema = use('Schema')
 class NoteTimelineSchema extends Schema {
   up() {
     this.create('note_timelines', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('note_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('notes')
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+      table
+        .enu('type', ['CREATED', 'UPDATED'])
+        .notNullable()
+        .collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
