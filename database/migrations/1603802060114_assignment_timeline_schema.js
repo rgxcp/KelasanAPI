6c7a6aa81f358c074111cdb57f09f604d3918773
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-27 21:06:23 +0700

    Mengisi kerangka migrasi assignment_timeline

--- database/migrations/1603802060114_assignment_timeline_schema.js
+++ database/migrations/1603802060114_assignment_timeline_schema.js
@@ -5,8 +5,25 @@ const Schema = use('Schema')
 class AssignmentTimelineSchema extends Schema {
   up() {
     this.create('assignment_timelines', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('assignment_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('assignments')
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+      table
+        .enu('type', ['CREATED', 'UPDATED'])
+        .notNullable()
+        .collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
