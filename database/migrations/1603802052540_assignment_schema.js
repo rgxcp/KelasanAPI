f6b126bdd13e3f5fdb8d555ad49713a8d5a40da5
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-29 01:21:42 +0700

    Mengisi kerangka migrasi assignment

--- database/migrations/1603802052540_assignment_schema.js
+++ database/migrations/1603802052540_assignment_schema.js
@@ -5,8 +5,35 @@ const Schema = use('Schema')
 class AssignmentSchema extends Schema {
   up() {
     this.create('assignments', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('classroom_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('classrooms')
+      table
+        .integer('folder_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('folders')
+      table
+        .integer('subject_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('subjects')
+      table.string('detail').notNullable().collate('utf8mb4_unicode_ci')
+      table.string('where_to_collect', 20).collate('utf8mb4_unicode_ci')
+      table.string('collector').collate('utf8mb4_unicode_ci')
+      table
+        .enu('type', ['INDIVIDUAL', 'GROUP'])
+        .notNullable()
+        .collate('utf8mb4_unicode_ci')
+      table.datetime('deadline')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
