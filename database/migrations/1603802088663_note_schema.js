17e61c6e5098ca4fc3f4607b01a3d9b10e6bb583
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-29 01:25:27 +0700

    Mengisi kerangka migrasi note

--- database/migrations/1603802088663_note_schema.js
+++ database/migrations/1603802088663_note_schema.js
@@ -5,8 +5,25 @@ const Schema = use('Schema')
 class NoteSchema extends Schema {
   up() {
     this.create('notes', (table) => {
-      table.increments()
-      table.timestamps()
+      table.increments('id')
+      table
+        .integer('classroom_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('classrooms')
+      table
+        .integer('folder_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('folders')
+      table
+        .text('detail', 'longtext')
+        .notNullable()
+        .collate('utf8mb4_unicode_ci')
+      table.timestamp('created_at').nullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
