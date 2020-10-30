3433a5393ea772f9a7ce7db48e688b8f6648b1d6
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-30 15:23:04 +0700

    Mengubah posisi route

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,24 @@
 const Route = use('Route')
 
 // Version 1
+// Assignment
+Route.group(() => {
+  Route.get(':id', 'AssignmentController.detail').middleware('classMember')
+  Route.get('today', 'AssignmentController.today').middleware('classMember')
+  Route.get('tomorrow', 'AssignmentController.tomorrow').middleware('classMember')
+  Route.get('next-week', 'AssignmentController.nextWeek').middleware('classMember')
+  Route.get('completed', 'AssignmentController.completed').middleware('classMember')
+  Route.get('uncompleted', 'AssignmentController.uncompleted').middleware('classMember')
+  Route.get(':id/timeline', 'AssignmentController.timeline').middleware('classMember')
+  Route.post('create', 'AssignmentController.create').middleware('classMember').validator('Assignment')
+  Route.post(':id/mark-as-completed', 'AssignmentController.markAsCompleted').middleware('classMember')
+  Route.put(':id/update', 'AssignmentController.update').middleware('classMember').validator('Assignment')
+  Route.delete(':id/delete', 'AssignmentController.delete').middleware('classLeader')
+  Route.delete(':id/mark-as-uncompleted', 'AssignmentController.markAsUncompleted').middleware('classMember')
+})
+  .prefix('api/v1/assignment')
+  .middleware('auth')
+
 // Classroom
 Route.group(() => {
   Route.get(':id', 'ClassroomController.detail').middleware('classMember')
@@ -49,6 +67,30 @@ Route.group(() => {
   .prefix('api/v1/folder')
   .middleware('auth')
 
+// Note
+Route.group(() => {
+  Route.get(':id', 'NoteController.detail').middleware('classMember')
+  Route.get(':id/timeline', 'NoteController.timeline').middleware('classMember')
+  Route.post('create', 'NoteController.create').middleware('classMember').validator('Note')
+  Route.put(':id/update', 'NoteController.update').middleware('classMember').validator('Note')
+  Route.delete(':id/delete', 'NoteController.delete').middleware('classLeader')
+})
+  .prefix('api/v1/note')
+  .middleware('auth')
+
+// Search
+Route.group(() => {
+  Route.get('all', 'SearchController.all')
+  Route.get('classroom', 'SearchController.classroom')
+  Route.get('class-member', 'SearchController.classMember')
+  Route.get('folder', 'SearchController.folder')
+  Route.get('subject', 'SearchController.subject')
+  Route.get('assignment', 'SearchController.assignment')
+  Route.get('note', 'SearchController.note')
+})
+  .prefix('api/v1/search')
+  .middleware(['auth', 'classMember'])
+
 // Subject
 Route.group(() => {
   Route.get(':id', 'SubjectController.detail').middleware('classMember')
@@ -60,35 +102,6 @@ Route.group(() => {
   Route.delete(':id/delete', 'SubjectController.delete').middleware('classLeader')
 })
   .prefix('api/v1/subject')
-  .middleware('auth')
-
-// Assignment
-Route.group(() => {
-  Route.get(':id', 'AssignmentController.detail').middleware('classMember')
-  Route.get('today', 'AssignmentController.today').middleware('classMember')
-  Route.get('tomorrow', 'AssignmentController.tomorrow').middleware('classMember')
-  Route.get('next-week', 'AssignmentController.nextWeek').middleware('classMember')
-  Route.get('completed', 'AssignmentController.completed').middleware('classMember')
-  Route.get('uncompleted', 'AssignmentController.uncompleted').middleware('classMember')
-  Route.get(':id/timeline', 'AssignmentController.timeline').middleware('classMember')
-  Route.post('create', 'AssignmentController.create').middleware('classMember').validator('Assignment')
-  Route.post(':id/mark-as-completed', 'AssignmentController.markAsCompleted').middleware('classMember')
-  Route.put(':id/update', 'AssignmentController.update').middleware('classMember').validator('Assignment')
-  Route.delete(':id/delete', 'AssignmentController.delete').middleware('classLeader')
-  Route.delete(':id/mark-as-uncompleted', 'AssignmentController.markAsUncompleted').middleware('classMember')
-})
-  .prefix('api/v1/assignment')
-  .middleware('auth')
-
-// Note
-Route.group(() => {
-  Route.get(':id', 'NoteController.detail').middleware('classMember')
-  Route.get(':id/timeline', 'NoteController.timeline').middleware('classMember')
-  Route.post('create', 'NoteController.create').middleware('classMember').validator('Note')
-  Route.put(':id/update', 'NoteController.update').middleware('classMember').validator('Note')
-  Route.delete(':id/delete', 'NoteController.delete').middleware('classLeader')
-})
-  .prefix('api/v1/note')
   .middleware('auth')
 
 // User
@@ -111,16 +124,3 @@ Route.group(() => {
 })
   .prefix('api/v1/user/self')
   .middleware('auth')
-
-// Search
-Route.group(() => {
-  Route.get('all', 'SearchController.all')
-  Route.get('classroom', 'SearchController.classroom')
-  Route.get('class-member', 'SearchController.classMember')
-  Route.get('folder', 'SearchController.folder')
-  Route.get('subject', 'SearchController.subject')
-  Route.get('assignment', 'SearchController.assignment')
-  Route.get('note', 'SearchController.note')
-})
-  .prefix('api/v1/search')
-  .middleware(['auth', 'classMember'])
