6d6337cecc4b473c8981cca1005b521055af890a
Rommy Gustiawan <rgcppp@gmail.com>
2020-11-04 15:16:07 +0700

    Mengisi kerangka middleware classroom leader

--- app/Middleware/ClassroomLeader.js
+++ app/Middleware/ClassroomLeader.js
@@ -1,7 +1,23 @@
 'use strict'
 
+const ClassMember = use('ClassMember')
+
 class ClassroomLeader {
-  async handle({ request }, next) {
+  async handle({ params: { id }, response, auth }, next) {
+    const userId = await auth.user.id
+
+    const classLeader = await ClassMember.query()
+      .where('classroom_id', id)
+      .where('user_id', userId)
+      .where('role', 'LEADER')
+      .first()
+
+    if (!classLeader) {
+      return response.status(401).json({
+        message: 'Unauthorized'
+      })
+    }
+
     await next()
   }
 }
