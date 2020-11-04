202354f0d60c052dcef9b9bf614540f119c72d81
Rommy Gustiawan <rgcppp@gmail.com>
2020-11-04 14:21:02 +0700

    Mengisi kerangka middleware classroom exist

--- app/Middleware/ClassroomExist.js
+++ app/Middleware/ClassroomExist.js
@@ -1,7 +1,21 @@
 'use strict'
 
+const Classroom = use('Classroom')
+
 class ClassroomExist {
-  async handle({ request }, next) {
+  async handle({ request, response }, next) {
+    const { invitation_code } = request.post()
+
+    const classroom = await Classroom.findBy('invitation_code', invitation_code)
+
+    if (!classroom) {
+      return response.status(404).json({
+        message: 'Not Found'
+      })
+    }
+
+    request.body.classroom_id = classroom.id
+
     await next()
   }
 }
