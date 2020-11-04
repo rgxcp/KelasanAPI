f6a4ca1ee3c478fe45c2a6ca2284e1d8d5c8abf2
Rommy Gustiawan <rgcppp@gmail.com>
2020-11-04 15:09:04 +0700

    Mengisi kerangka middleware classroom member

--- app/Middleware/ClassroomMember.js
+++ app/Middleware/ClassroomMember.js
@@ -1,7 +1,22 @@
 'use strict'
 
+const ClassMember = use('ClassMember')
+
 class ClassroomMember {
-  async handle({ request }, next) {
+  async handle({ params: { id }, response, auth }, next) {
+    const userId = await auth.user.id
+
+    const classMember = await ClassMember.query()
+      .where('classroom_id', id)
+      .where('user_id', userId)
+      .first()
+
+    if (!classMember) {
+      return response.status(401).json({
+        message: 'Unauthorized'
+      })
+    }
+
     await next()
   }
 }
